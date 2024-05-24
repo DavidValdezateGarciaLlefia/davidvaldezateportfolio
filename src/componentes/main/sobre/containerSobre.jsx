a56bca41c3ab4ee44c88f88eb9b3c48727496fa0
DavidValdezateGarciaLlefia <120660026+DavidValdezateGarciaLlefia@users.noreply.github.com>
2024-05-24 15:47:54 +0200

    Update containerSobre.jsx

--- src/componentes/main/sobre/containerSobre.jsx
+++ src/componentes/main/sobre/containerSobre.jsx
@@ -8,13 +8,14 @@ export function ContainerSobre() {
     <div className="flex flex-col justify-center items-center min-h-screen bg-black">
       <div className="container mx-auto px-4 py-6">
         <Titulo titulo="SOBRE MÍ" />
-        <div className="flex flex-col justify-between items-center w-full">
-          <Conoceme
-            titulo="Así es David"
-            parrafo1="Soy un <strong>Programador FullStack</strong> dedicado al desarrollo de aplicaciones Web, manejando tanto el front-end como el back-end para asegurar el éxito del producto."
-            parrafo2="A lo largo de mi carrera, he adquirido múltiples habilidades en <strong>desarrollo web</strong> y me esfuerzo por perfeccionar mis conocimientos constantemente."
-            parrafo3="Estoy siempre abierto a nuevas oportunidades de <strong>trabajo</strong>, dispuesto a colaborar, aprender y crecer en entornos desafiantes. Si encuentras una oportunidad que se alinea con mis habilidades, no dudes en contactarme."
-          />
+        <div className="flex flex-col justify-between items-center w-full text-white">
+        <Conoceme
+  titulo="Perfil Profesional de David"
+  parrafo1="Soy un <strong>Desarrollador FullStack</strong> comprometido con la creación de aplicaciones web robustas, manejando con destreza tanto el frontend como el backend para garantizar la excelencia y funcionalidad del producto final."
+  parrafo2="Durante mi carrera profesional, he consolidado una amplia gama de habilidades técnicas en <strong>desarrollo web</strong>, lo cual me permite adaptarme y contribuir eficazmente en diversos proyectos tecnológicos."
+  parrafo3="Estoy continuamente en búsqueda de oportunidades para <strong>trabajar</strong> que me permitan colaborar en entornos estimulantes, aprender nuevas tecnologías y contribuir al éxito de iniciativas ambiciosas. Si tienes una oportunidad que se ajusta a mi perfil, estaré encantado de explorar cómo podemos colaborar."
+  imageUrl="https://i.imgur.com/X82wxBu.png"
+/>
           <ContainerHabilidades titulo="Mis habilidades" />
         </div>
       </div>
