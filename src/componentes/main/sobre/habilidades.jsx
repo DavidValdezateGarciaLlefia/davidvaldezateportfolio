d94f9fc4c63b8d8bd1ef164b3a884725f2800fc6
DavidValdezateGarciaLlefia <120660026+DavidValdezateGarciaLlefia@users.noreply.github.com>
2024-05-25 15:25:10 +0200

    Update habilidades.jsx

--- src/componentes/main/sobre/habilidades.jsx
+++ src/componentes/main/sobre/habilidades.jsx
@@ -1,6 +1,6 @@
 export function Habilidades({ habilidad }) {
   return (
-    <div className="bg-red-500 text-white py-2 px-4 rounded shadow m-2 text-center cursor-default animate-pulse">
+    <div style={{ backgroundColor: '#FF6B6B' }} className="text-white py-2 px-4 rounded shadow m-2 text-center cursor-default animate-pulse">
       {habilidad}
     </div>
   );
